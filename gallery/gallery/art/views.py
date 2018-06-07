a9e0cf72051a51cd293bca8a8c2d82b7cf6303a0
claudianjeri <claudianjeri04@gmail.com>
2018-06-07 11:42:47 +0300

    Initial Commit

---
+++ gallery/gallery/art/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse #this is returns a response to a user
+
+# Create your views here.
+
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/claudia/Documents/moringa/django-projects/gallery/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
