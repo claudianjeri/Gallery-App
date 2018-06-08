a73c947a651174b25977e4a344b8e2c50c94f799
claudianjeri <claudianjeri04@gmail.com>
2018-06-08 12:50:18 +0300

    setting up views

--- arts/views.py
+++ arts/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse #this is returns a response to a user
 
 # Create your views here.
 
 def welcome(request):
-    title = 'Heeeeey'
-    return render( title =title )
+    
+    return HttpResponse('Gallery')
+
+def search_results(request):
+    pass
+
+def image(request):
+    pass
+
+def filter_location(request):
+    pass
+
+
 
