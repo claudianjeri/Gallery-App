87b8e777cff816943a7b4c6fd9f15c5abd2335fb
claudianjeri <claudianjeri04@gmail.com>
2018-06-09 23:16:09 +0300

    registered models

--- arts/tests.py
+++ arts/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from .models import Location, Category, Image
 # Create your tests here.
+
+# class LocationTestClass(TestCase):
+#     def setUp(self):
+#         self.
