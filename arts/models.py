3a3e4c9e7ced1622ffa7f03944091b4049c02bda
claudianjeri <claudianjeri04@gmail.com>
2018-06-09 23:18:42 +0300

    installed pillow

--- arts/models.py
+++ arts/models.py
@@ -25,34 +25,34 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
-# class Image(models.Model):
-#     name = models.CharField(max_length=30)
-#     description = models.TextField()
-#     upload_date = models.DateTimeField(auto_now_add=True)
-#     image_url = models.ImageField(upload_to = 'categories/',null=True,blank=True)
-#     location = models.ManyToManyField(Location)
-#     category = models.ManyToManyField(Category)
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    image_url = models.ImageField(upload_to = 'categories/',null=True,blank=True)
+    location = models.ManyToManyField(Location)
+    category = models.ManyToManyField(Category)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     def save_image(self):
-#         self.save()
+    def save_image(self):
+        self.save()
 
-#     def delete_image(self):
-#         self.delete()
+    def delete_image(self):
+        self.delete()
 
-#     @classmethod
-#     def get_images(cls):
-#         images = Image.objects.all()
-#         return images
+    @classmethod
+    def get_images(cls):
+        images = Image.objects.all()
+        return images
 
-#     @classmethod
-#     def filter_by_location(cls,id):
-#         images = Image.objects.filter(id=location.id)
-#         return images
+    @classmethod
+    def filter_by_location(cls,id):
+        images = Image.objects.filter(id=location.id)
+        return images
 
-#     @classmethod
-#     def search_by_category(cls, search_term):
-#         category = cls.objects.filter(category__category_name__icontains=search_term)
-#         return category 
+    @classmethod
+    def search_by_category(cls, search_term):
+        category = cls.objects.filter(category__category_name__icontains=search_term)
+        return category 
